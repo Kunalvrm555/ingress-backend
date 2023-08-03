bae8bb420d48a04590542b69e22f93b4a1174900
Kunal Verma <kunalvrm555@gmail.com>
2023-08-03 21:29:53 +0530

    refactor: improve student route code
    This commit refactors the GetStudent handler in the studentRoutes.go file.Additionally, it moves the logic for formatting the check-in and checkout times into a separate function called parseDatabaseTime.
    
    The GetStudent handler now retrieves the student's information from the database, updates the checkout time for the student, inserts a new log entry if it doesn't exist, and commits the transaction. The check-in and checkout times are retrieved from the logs table and formatted as "03:04 PM" format.
    
    The response content type is set to JSON, and the student object is encoded and sent as the response. Any errors encountered during the process will result in returning a "Server error" response.
    
    The student's information is also logged to the console.

--- routes/studentRoutes.go
+++ routes/studentRoutes.go
@@ -33,14 +33,17 @@ func init() {
 	}
 }
 
+// GetStudent handles the GET request for retrieving student information
 func GetStudent(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	rollNo := params["rollno"]
 
 	fmt.Println(params["rollno"])
 
+	// Introducing a small delay to simulate some processing time
 	time.Sleep(2 * time.Second)
 
+	// Begin a transaction to update the checkout time for the student
 	tx, err := db.Begin()
 	if err != nil {
 		http.Error(w, "Server error.", http.StatusInternalServerError)
@@ -48,6 +51,7 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Record the current time as the checkout time for the student
 	currentTime := time.Now()
 	currentDate := currentTime.Format("2006-01-02")
 	_, err = tx.Exec("UPDATE logs SET checkouttime = $1 WHERE rollno = $2 AND DATE(checkintime) = DATE($1)", currentTime, rollNo)
@@ -58,9 +62,8 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Retrieve the student's name, department, and type from the database
 	row := db.QueryRow("SELECT name, dept, type FROM students WHERE rollno = $1", rollNo)
-
-	//type is a keyword in Go, so we use type_ instead
 	var name, dept, type_ string
 	err = row.Scan(&name, &dept, &type_)
 	if err != nil {
@@ -72,6 +75,7 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Insert a new log entry for the student if it doesn't exist for the current date
 	_, err = tx.Exec("INSERT INTO logs (rollno, name, dept, type, checkintime) SELECT $1, $2, $3, $4, $5 WHERE NOT EXISTS (SELECT 1 FROM logs WHERE rollno = $1 AND DATE(checkintime) = $6)", rollNo, name, dept, type_, currentTime, currentDate)
 	if err != nil {
 		tx.Rollback()
@@ -80,6 +84,7 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Commit the transaction
 	err = tx.Commit()
 	if err != nil {
 		http.Error(w, "Server error.", http.StatusInternalServerError)
@@ -87,9 +92,8 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Retrieve the check-in time and checkout time for the student
 	row = db.QueryRow("SELECT checkintime, checkouttime FROM logs WHERE rollno = $1 AND DATE(checkintime) = DATE($2)", rollNo, currentTime)
-
-	// Use sql.NullString to handle NULL values for checkouttime
 	var checkInTime, checkoutTime sql.NullString
 	err = row.Scan(&checkInTime, &checkoutTime)
 	if err != nil {
@@ -98,38 +102,54 @@ func GetStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// If checkoutTime is NULL, set an empty string for the response
-	var checkoutTimeStr string
-	if checkoutTime.Valid {
-		checkoutTimeStr = checkoutTime.String
-
-		// Format the checkoutTime as "03:04 PM" format
-		parsedTime, err := time.Parse(time.RFC3339Nano, checkoutTimeStr)
-		if err != nil {
-			http.Error(w, "Server error.", http.StatusInternalServerError)
-			fmt.Println(err, "6")
-			return
-		}
-		checkoutTimeStr = parsedTime.Format("03:04 PM")
-	}
-
-	// Format the checkInTime as "03:04 PM" format
-	parsedTime, err := time.Parse(time.RFC3339Nano, checkInTime.String)
+	// Format the check-in time as "03:04 PM" format
+	parsedCheckInTime, err := parseDatabaseTime(checkInTime.String)
 	if err != nil {
 		http.Error(w, "Server error.", http.StatusInternalServerError)
-		fmt.Println(err, "7")
+		fmt.Println(err, "6")
 		return
 	}
-	checkInTimeFormatted := parsedTime.Format("03:04 PM")
+	checkInTimeFormatted := parsedCheckInTime.Format("03:04 PM")
 
+	// Format the checkout time as "03:04 PM" format
+	var checkoutTimeStr string
+	if checkoutTime.Valid {
+		parsedCheckoutTime, err := parseDatabaseTime(checkoutTime.String)
+		if err != nil {
+			http.Error(w, "Server error.", http.StatusInternalServerError)
+			fmt.Println(err, "7")
+			return
+		}
+		checkoutTimeStr = parsedCheckoutTime.Format("03:04 PM")
+	}
+
+	// Create the student object to be returned in the response
 	student := Student{
 		RollNo:       rollNo,
 		Name:         name,
 		CheckInTime:  checkInTimeFormatted,
 		CheckoutTime: checkoutTimeStr,
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(student)
 
+	// Set the response content type to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Encode the student object and send it as the response
+	err = json.NewEncoder(w).Encode(student)
+	if err != nil {
+		http.Error(w, "Server error.", http.StatusInternalServerError)
+		fmt.Println(err, "8")
+		return
+	}
+
+	// Log the student information
 	fmt.Println(student.RollNo, student.Name, student.CheckInTime, student.CheckoutTime)
+}
+
+// parseDatabaseTime parses the time string stored in the database (RFC3339Nano format)
+// and returns it as a time.Time object.
+func parseDatabaseTime(timeStr string) (time.Time, error) {
+	// The format used in the database for the time is RFC3339Nano
+	// e.g., "2023-08-03T21:20:56.051486Z"
+	return time.Parse(time.RFC3339Nano, timeStr)
 }

9deaa707169d23ac1b2911e68e17744f66bf300b
Kunal Verma <kunalvrm555@gmail.com>
2023-08-08 19:05:02 +0530

    fix: retry database connection
    Refactor the `Connect` function to include retries when connecting to the database. If a connection attempt fails, it will be retried up to a maximum of 5 times before returning an error. Each failed attempt is logged, and there is a 2-second delay between retries. If all retries fail, a "Connection failure after retries" message will be logged, indicating the inability to establish a connection to the database.

--- database/database.go
+++ database/database.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -15,20 +16,39 @@ var (
 	once sync.Once
 )
 
+const maxRetries = 5
+
+func connectAndPing() (*sql.DB, error) {
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=5432 dbname=ingress sslmode=disable",
+		os.Getenv("POSTGRES_USERNAME"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"))
+
+	tmpDB, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to the database: %v", err)
+	}
+
+	for i := 0; i < maxRetries; i++ {
+		err = tmpDB.Ping()
+		if err == nil {
+			return tmpDB, nil
+		}
+
+		log.Printf("Failed to connect to database (attempt %d/%d). Retrying in 2 seconds...\n", i+1, maxRetries)
+		time.Sleep(time.Second * 2)
+	}
+
+	tmpDB.Close()
+	return nil, fmt.Errorf("Failed to connect to database after %d tries: %v", maxRetries, err)
+}
+
 func Connect() *sql.DB {
 	once.Do(func() {
 		var err error
-		connStr := fmt.Sprintf("user=%s password=%s host=%s port=5432 dbname=ingress sslmode=disable",
-			os.Getenv("POSTGRES_USERNAME"),
-			os.Getenv("POSTGRES_PASSWORD"),
-			os.Getenv("POSTGRES_HOST"))
-		db, err = sql.Open("postgres", connStr)
+		db, err = connectAndPing()
 		if err != nil {
-			log.Fatalf("Failed to connect to the database: %v", err)
-		}
-
-		if err = db.Ping(); err != nil {
-			log.Fatalf("Failed to ping database: %v", err)
+			log.Fatalf("Connection failure after retries: %v", err)
 		}
 	})
 	return db
